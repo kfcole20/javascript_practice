b71f47d1c1a86de7fb4b4ffe88d68a3b780a5117
Kevin Cole <kfcole20@gmail.com>
2021-09-01 10:03:56 -0500

    Finished last problems of fundmentals_1

--- fundamentals_1.js
+++ fundamentals_1.js
@@ -115,19 +115,51 @@ incoming(100)
 incoming(myVar)
 
 // Add odd integers from -300,000 to 300,000, and console.log the final sum. Is there a shortcut?
-
- 
-
-// Countdown by Fours
+var sum=0;
+for(var i=-300000; i<=30000;i++){
+    if(i%2 !=0){
+        sum+=i
+    }
+}
+console.log(sum)
 // Log positive numbers starting at 2016, counting down by fours (exclude 0), without a FOR loop.
+var i=2016;
+while(i>0){
+    console.log(i)
+    i-=4
+}
 
- 
-
-// Flexible Countdown
 // Based on earlier “Countdown by Fours”, given lowNum, highNum, mult, print multiples of mult from highNum down to lowNum, using a FOR. For (2,9,3), print 9 6 3 (on successive lines).
-
- 
+function flexCount(lowNum, highNum, mult){
+    for(var i=highNum; i>= lowNum; i--)
+    {
+        if(i%mult ==0)
+        {
+            console.log(i)
+        }
+    }
+}
+flexCount(0, 100, 2)
+flexCount(12, 96, 8)
 
 // The Final Countdown
 // This is based on “Flexible Countdown”. The parameter names are not as helpful, but the problem is essentially identical; don’t be thrown off! Given 4 parameters (param1,param2,param3,param4), print the multiples of param1, starting at param2 and extending to param3. One exception: if a multiple is equal to param4, then skip (don’t print) it. Do this using a WHILE. Given (3,5,17,9), print 6,12,15 (which are all of the multiples of 3 between 5 and 17, and excluding the value 9).
+//(param1 mult,param2/start,param3/end,param4/exception)
+function flexCount2(param1, param2, param3, param4){
+    for(var i=param2; i>= param3; i--)
+    {
+        if(i%param1 ==0)
+        {
+            if(i%param4 ==0 ){
+                continue;
+            }
+            else{
+                console.log(i);
+            }
+            
+        }
+    }
+}
 
+flexCount2(2, 100, 0, 4)
+flexCount2(8, 800, 250, 5)
